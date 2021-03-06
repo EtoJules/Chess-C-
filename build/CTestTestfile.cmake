8f412f05f8676a33e864d353675743533597613e
Andrew <mudryk200@gmail.com>
2021-03-06 18:48:43 +0100

    gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrew/MY/PWR/Algorytmy/GameCMake
-# Build directory: /home/andrew/MY/PWR/Algorytmy/GameCMake/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrew/MY/PWR/Algorytmy/GameCMake/src
-# Build directory: /home/andrew/MY/PWR/Algorytmy/GameCMake/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
